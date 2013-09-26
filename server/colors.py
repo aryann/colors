aa4cf03816ef3e5ab048378a15d2d36bd397e0f4
Aryan Naraghi <aryan.naraghi@gmail.com>
2013-09-25 22:20:28 -0700

    Simplified some of the logic and implemented support for hanging GET requests. Th latter will increase the responsiveness of the system.

--- server/colors.py
+++ server/colors.py
@@ -2,137 +2,183 @@
 
 __author__ = 'Aryan Naraghi (aryan.naraghi@gmail.com)'
 
+import hashlib
+import httplib
 import json
 import logging
 import os
 import supported_colors
 import webapp2
+import time
 
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
-# We only need to store one entity--this constant defines its key.
-DATA_KEY = '0'
 
-# HTTP eror code(s)
-HTTP_BAD_REQUEST = 400
+_HANGING_GET_DURATION_SEC = 25
 
 # Allowed value ranges
-MIN_COLORS_ALLOWED = 1
-MAX_COLORS_ALLOWED = 10
-MIN_DURATION_MS = 0
-MAX_DURATION_MS = 4000  # 4 seconds
-
-DEFAULT = {
-    'display_duration_ms': 1000,
-    'fadeout_duration_ms': 1000,
-    'colors': ['FFFFFF', 'FF00FF']}
-
-SCHEMA = {
-    'display_duration_ms': int,
-    'fadeout_duration_ms': int,
-    'colors': list}
+_MIN_COLORS_ALLOWED = 1
+_MAX_COLORS_ALLOWED = 10
+_MIN_DURATION_MS = 0
+_MAX_DURATION_MS = 4000  # 4 seconds
 
 
-def has_valid_structure(payload, schema=SCHEMA):
-    """An "okay-enough" way of verifying that the given payload
-    matches the expected schema.
-    """
-    if (not isinstance(payload, dict) or
-        len(payload) != len(schema)):
-        return False
-    for key, type in schema.iteritems():
-        if (key not in payload or
-            not isinstance(payload[key], type)):
-            return False
-    return True
+_EXPECTED_KEYS = [
+    'display_duration_ms',
+    'fadeout_duration_ms',
+    'colors',
+]
 
 
-class Data(db.Model):
+class Client(db.Model):
+    user_agent = db.StringProperty()
+    ip = db.StringProperty()
+
+    @staticmethod
+    def get_or_create(user_agent, ip):
+        h = hashlib.sha1()
+        h.update(str(user_agent))
+        h.update(str(ip))
+        return Client.get_or_insert(
+            key_name=h.hexdigest(),
+            user_agent=user_agent,
+            ip=ip)
+
+
+class ColorConfig(db.Model):
+    insert_time = db.DateTimeProperty(auto_now=True)
+
     colors = db.StringListProperty()
     display_duration_ms = db.IntegerProperty()
     fadeout_duration_ms = db.IntegerProperty()
 
+    client = db.ReferenceProperty()
+
+    @staticmethod
+    def default():
+        return ColorConfig(
+            display_duration_ms=1000,
+            fadeout_duration_ms=1000,
+            colors=['FFFFFF', 'FF00FF'])
+
     def to_dict(self):
-        return {key: getattr(self, key) for key in self.properties()}
+        return {
+            'colors': self.colors,
+            'display_duration_ms': self.display_duration_ms,
+            'fadeout_duration_ms': self.fadeout_duration_ms,
+        }
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.key() == other.key()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
-class RequestHandler(webapp2.RequestHandler):
-    """Base class for handlers."""
-
-    def return_error(self, msg, *args, **kwargs):
-        self.response.clear()
-        self.response.write(msg.format(*args, **kwargs))
-        if msg[-1] != '\n':
-            self.response.write('\n')
-        self.response.set_status(HTTP_BAD_REQUEST)
-
-    def return_json(self, payload):
-        self.response.clear()
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.write(json.dumps(payload))
-        self.response.write('\n')
-
-
-class ColorHandler(RequestHandler):
+class ColorHandler(webapp2.RequestHandler):
     """Handler for setting and viewing the colors."""
 
-    def get(self):
-        data = Data.get_by_key_name(DATA_KEY)
-        if not data:
-            data = Data(key_name=DATA_KEY, **DEFAULT)
-            data.put()
+    def get_latest(self):
+        """Returns the latest color record or None."""
+        result = list(ColorConfig.all().order('-insert_time').run(limit=1))
+        if not result:
+            return None
+        return result[0]
 
-        if self.request.get('format') == 'json':
-            self.return_json(data.to_dict())
-        else:
-            display = str(data.display_duration_ms)
-            fadeout = str(data.fadeout_duration_ms)
-            for color in data.colors:
-                self.response.write(':'.join((color, fadeout, display)))
-                self.response.write(' ')
-            self.response.write('\n')
+    def get(self):
+        """Handles requests to get the color configurations as JSON.
+
+        Clients can pass in hang=1 as a GET parameter to cause the
+        request to hang until configs have changed.
+        """
+        current = self.get_latest()
+        if not current:
+            current = ColorConfig.default()
+            current.put()
+        new = current
+
+        if self.request.get('hang') == '1':
+            start = time.time()
+            while True:
+                new = self.get_latest()
+                if new != current:
+                    break
+
+                time.sleep(1)
+                if time.time() - start > _HANGING_GET_DURATION_SEC:
+                    break
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(new.to_dict()))
+        self.response.write('\n')
+
+    def write_error(self, msg):
+        """Writes the given message to self.response."""
+        self.response.clear()
+        self.response.write(msg)
+        self.response.write('\n')
+        self.response.set_status(httplib.BAD_REQUEST)
 
     def post(self):
+        """Handles requests to change the color configuration."""
         logging.info('Received POST request: %s', self.request.body)
 
-        self.response.headers['Content-Type'] = 'text/plain'
         payload = None
         try:
             payload = json.loads(self.request.body)
         except ValueError:
-            pass
-
-        if not has_valid_structure(payload):
-            self.return_error('Request was malformed.')
+            self.return_error('Could not parse POST body as JSON.')
             return
 
-        for key in ('display_duration_ms', 'fadeout_duration_ms'):
+        for key in _EXPECTED_KEYS:
+            if key not in payload:
+                self.return_error('Expected key missing: {0}'.format(key))
+                return
+
+        for key in ['display_duration_ms', 'fadeout_duration_ms']:
             duration = payload[key]
-            if duration not in xrange(MIN_DURATION_MS, MAX_DURATION_MS + 1):
+            if duration not in xrange(_MIN_DURATION_MS, _MAX_DURATION_MS + 1):
                 self.return_error(
-                    '{key} must be in the range [{min}, {max}]. Received: {val}',
-                    key=key,
-                    min=MIN_DURATION_MS,
-                    max=MAX_DURATION_MS,
-                    val=duration)
+                    '{key} must be in the range [{min}, {max}]. Received: {val}'
+                    .format(
+                        key=repr(key),
+                        min=_MIN_DURATION_MS,
+                        max=_MAX_DURATION_MS,
+                        val=duration))
                 return
 
         colors = payload['colors']
-        if len(colors) not in xrange(MIN_COLORS_ALLOWED,
-                                     MAX_COLORS_ALLOWED + 1):
-            self.return_error('Received invalid number of colors: {0}',
-                             len(colors))
+        if len(colors) not in xrange(_MIN_COLORS_ALLOWED,
+                                     _MAX_COLORS_ALLOWED + 1):
+            self.return_error(
+                'Number of colors must be in range [{min}, {max}]. '
+                'Received: {num_colors}'.format(
+                    min=_MIN_COLORS_ALLOWED,
+                    max=_MAX_COLORS_ALLOWED,
+                    num_colors=len(colors)))
             return
 
         colors = [color.upper() for color in colors]
         for color in colors:
             if color not in supported_colors.COLORS:
-                self.return_error('Unrecognized color: {0}', color)
+                self.return_error('Unrecognized color: {0}'.format(color))
                 return
 
-        data = Data(key_name=DATA_KEY, **payload)
-        data.put()
+        client = Client.get_or_create(
+            user_agent=self.request.user_agent,
+            ip=self.request.remote_addr)
+        client.put()
+        config = ColorConfig(
+            display_duration_ms=payload['display_duration_ms'],
+            fadeout_duration_ms=payload['fadeout_duration_ms'],
+            colors=colors,
+            client=client)
+        config.put()
+
+        self.response.set_status(httplib.OK)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -140,10 +186,10 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         template_vals = {
-            'colors_min': MIN_COLORS_ALLOWED,
-            'colors_max': MAX_COLORS_ALLOWED,
-            'duration_min': MIN_DURATION_MS,
-            'duration_max': MAX_DURATION_MS,
+            'colors_min': _MIN_COLORS_ALLOWED,
+            'colors_max': _MAX_COLORS_ALLOWED,
+            'duration_min': _MIN_DURATION_MS,
+            'duration_max': _MAX_DURATION_MS,
             'supported_colors': supported_colors.COLORS}
         self.response.write(template.render(path, template_vals))
 
